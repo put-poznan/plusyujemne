93ea055f2454c544f1bcdea153c59af093401198
Xevaquor <xevaquor@hotmail.com>
2014-05-12 21:36:42 +0200

    Basic template before refactor

--- src/main.c
+++ src/main.c
@@ -1,93 +1,81 @@
 
 #include <allegro5\allegro5.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
+#include <allegro5\allegro_primitives.h>
+#include <allegro5\allegro_image.h>
+
 #include <stdio.h>
 #include "LinkedList.h"
 
 void init(ALLEGRO_DISPLAY **disp, int xres, int yres)
 {
 	al_init();
-	*disp = al_create_display(xres, yres);	
-	
+	al_install_keyboard();
+	al_init_font_addon();
+	al_init_primitives_addon();
+	al_init_image_addon();
+	al_install_mouse();
+
+	*disp = al_create_display(xres, yres);
 }
-
-void printll(struct LinkedList *ll)
-{
-	struct LinkedListNode *iter = ll->head;
-	while(iter != NULL)
-	{
-		puts(iter->val);
-		iter = iter->next;
-	}
-}
-
-void testll(void)
-{
-	struct LinkedList *ll = CreateLinkedList();
-	char* a = "tusk";
-	char* b = "jkm";
-	char* c = "krul";
-	char* d = "axelio";
-	char* e = "lech";
-
-	PrependLL(ll, a);
-	PrependLL(ll, b);
-	PrependLL(ll, c);
-	PrependLL(ll, d);
-	PrependLL(ll, e);
-
-	printll(ll);
-
-	ReleaseLinkedList(ll);
-	return;
-
-	RemoveLL(ll, a);
-	RemoveLL(ll, d);
-	RemoveLL(ll, b);
-	printll(ll);
-	RemoveLL(ll, c);
-	RemoveLL(ll, e);
-
-	printll(ll);
-	ReleaseLinkedList(ll);
-}
-
-
 
 
 int main(int argc, char ** argv)
 {
-	argc;
-	argv;
-	testll();
-	//return 0;
 
-
-	puts("start while");
-	fflush(stdout);
 	ALLEGRO_DISPLAY *display = NULL;
-	
-   ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 
-	init(&display,800,600);
-   
-    al_install_keyboard();
-	
+	init(&display, 800, 600);
+	ALLEGRO_FONT *arial = al_create_builtin_font();
+
+	ALLEGRO_BITMAP *bolek = al_load_bitmap("gfx/bolek.png");
+
+
 	ALLEGRO_KEYBOARD_STATE KeyboardState;
-	puts("przed while");
-	fflush(stdout);
-	while(1)
+	ALLEGRO_MOUSE_STATE MouseState;
+
+	double dt = 0.0;
+	double lastUpdateTime = al_current_time();
+	double accumulator = 0;
+	double TIME_STEP = 1. / 30;
+
+	double x = 0, y = 0;
+
+	while (1)
 	{
-		
+		dt = al_current_time() - lastUpdateTime;
+		lastUpdateTime += dt;
+		accumulator += dt;
+
+		al_get_mouse_state(&MouseState);
 		al_get_keyboard_state(&KeyboardState);
-		if(al_key_down(&KeyboardState, ALLEGRO_KEY_ESCAPE))
+		if (al_key_down(&KeyboardState, ALLEGRO_KEY_ESCAPE))
 			break;
+
+		while (accumulator > TIME_STEP)
+		{
+			if (al_key_down(&KeyboardState, ALLEGRO_KEY_LEFT))
+				x -= 1;
+			if (al_key_down(&KeyboardState, ALLEGRO_KEY_RIGHT))
+				x += 1;
+			if (al_key_down(&KeyboardState, ALLEGRO_KEY_UP))
+				y -= 1;
+			if (al_key_down(&KeyboardState, ALLEGRO_KEY_DOWN))
+				y += 1;
+			accumulator -= TIME_STEP;
+		}
+		//render
+
+		al_clear_to_color(al_map_rgb(0x77, 0, 255));
+		al_draw_filled_circle(200, 200, 100,al_map_rgb(127,127,127));
+
+		al_draw_bitmap(bolek, x, y, 0);
 		
-		
-		
-		al_clear_to_color(al_map_rgb(0x77,0,255));
+		al_draw_text(arial, al_map_rgb(255, 255, 255), 200, 200, 0, "#walesacontent");
 		al_flip_display();
 	}
-	puts("po while");;
 
 	al_destroy_display(display);
 
