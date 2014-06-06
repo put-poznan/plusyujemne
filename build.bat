f57d739fe7fad04701c8e295ae4374601d3e339c
Xevaquor <xevaquor@hotmail.com>
2014-06-06 08:20:54 +0200

    Updated build script

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Building project...
 rem liballegro-5.0.10-monolith-md.a
 gcc src/main.c src/LinkedList.c src/util.c src/init.c src/input.c src/config.c ^
 src/AnimatedSprite.c src/Player.c src/Bullet.c src/ImageManager.c src/Background.c ^
-src/hud.c ^
+src/hud.c src/Enemy.c ^
 -o bin/dd.exe ^
 -Wall -Wextra -g ^
 -lallegro-5.0.10-monolith-md 
